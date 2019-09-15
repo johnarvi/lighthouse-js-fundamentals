0b255cb89787b4eb0d8430bb18ce98db08931dce
johnarvi <arvind.john@mail.utoronto.ca>
2019-09-15 02:04:30 +0000

    use a function to return a boolean if its even or not

---
+++ even.js
@@ -0,0 +1,20 @@
+// takes a number and returns a boolean 
+
+/*
+const isEven  = function (num) {
+  return num % 2 === 0; // returns a true boolean if no remainder (i.e even)
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+*/
+// above code simplified
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
