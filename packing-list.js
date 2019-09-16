0cf6eb9ec8b50668228163f33a540da1d05a1f5b
johnarvi <arvind.john@mail.utoronto.ca>
2019-09-16 02:28:12 +0000

    Using a WHILE loop to index through every item in array

--- packing-list.js
+++ packing-list.js
@@ -1,5 +1,7 @@
-const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+/*
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
@@ -7,3 +9,9 @@ for (let i = 0; i < packingList.length; i++) {
 
 packingList[3];
 
+*/
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
